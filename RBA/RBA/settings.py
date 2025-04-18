b84be25d8b1cf7cdcb3ddd2bdcd55e925f2167f0
alina <alinavahruseva29038@gmail.com>
2025-04-18 20:50:13 +0700

    вроде как подключила бд к проекту dbeaver и postgresql(по совету) и написала такой набросок главной страницы без стилей без всего

--- RBA/RBA/settings.py
+++ RBA/RBA/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'catalog',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'RBA.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rba_db',
+        'USER': 'postgres',
+        'PASSWORD': '1145',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
